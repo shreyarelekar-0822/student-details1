fd4f09a64108a28899c43ed00dd45ec324c6cb43
shreyarelekar-0822 <shreyarelekar0822@gmail.com>
2025-11-22 12:05:46 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,10 +1,10 @@
 import sys
-if len(sys.avgv)!=3:
+if len(sys.argv)!=3:
   print("usage:python student.py<name><rollno>")
   sys.exit(1)
   #sys.argv[0]is always the program name
 script_name = sys.argv[0]
-name=sys.avgv[1]
+name=sys.argv[1]
 
 print("script Name:",script_name)
 print("student Name:",name)
